563a6c56b2a63e7dd3225fa0ef1d0339ae5491c4
Piyush Raikwar <piyushraikwar555@gmail.com>
2020-12-02 10:48:48 +0530

    Apply ativations, support a(t-1), shift to cpu backend for torch

--- rollout.py
+++ rollout.py
@@ -1,10 +1,14 @@
-from gym.envs.box2d.car_racing import *
-import torchvision.transforms as transforms
+import warnings
+warnings.filterwarnings("ignore")
+
 import torch
 import numpy as np
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from gym.envs.box2d.car_racing import *
 
-device = torch.device('cuda')
-tensorize = transforms.Compose([transforms.ToTensor(),])
+
+device = torch.device('cpu')
 
 def Rollout(pca, cont, render=False, return_frames=False):
     assert cont.__name__ == 'global'
@@ -14,6 +18,7 @@ def Rollout(pca, cont, render=False, return_frames=False):
     obs = env.reset()
     total_reward = 0
     limit = 800
+    action = np.array([1, 0, 0], dtype=np.float64)
     time = 0
     frames = []
     while True:
@@ -22,11 +27,16 @@ def Rollout(pca, cont, render=False, return_frames=False):
         obs = obs.reshape(1, -1)
         obs = obs / 255
         z = pca.transform(obs)
-        z = tensorize(z).to(device)
+        z = torch.cat([torch.tensor(z).double(), torch.tensor(action).double().unsqueeze(0)], dim=1).to(device)
         action = cont(z)
-        obs, reward, done, _ = env.step(action.squeeze().detach().cpu().numpy())
+        action = action[0]
+        action[0] = F.tanh(action[0])
+        action[1] = F.sigmoid(action[1])
+        action[2] = F.sigmoid(action[2])
+        action = action.detach().cpu().numpy()
+        obs, reward, done, _ = env.step(action)
         if return_frames:
-            frames.append(obs)# clipping??
+            frames.append(obs)  # clipping??
         total_reward += reward
         
         if done or time > limit:
